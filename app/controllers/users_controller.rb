71f26f695e652dcef88612c3c63be1f1c5b48573
miyu <60389741+miyuu890@users.noreply.github.com>
2020-02-10 15:38:01 +0000

    Finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,78 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show, :edit,:update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end  
+
   def show
-    @user = User.find(params[:id])
-    
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
-      
     else
       render :new
     end
-  end  
-  
+  end
+
   def edit
-    @user = User.find(params[:id])
-  end  
-  
-  
+  end
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
-      
     else
-      render :edit
-    end  
+      render :edit      
+    end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデーターを削除しました。"
+    redirect_to users_url
+  end  
+
   private
-  
-  def user_params
-    params.require(:user).permit(:name,:email, :password, :password_confirmation)
-  end  
-  
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless @user == current_user
-  end  
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+       store_location
+       flash[:danger] = "ログインしてください。"
+       redirect_to login_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end  
 end
